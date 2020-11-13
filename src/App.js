febdaad83240a26f943a9ea56cb2e6e60af4069f
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-13 15:09:41 -0500

    render all data and relationships from db

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [songs, setSongs] = useState([]);
+	const [playlists, setPlaylists] = useState([]);
+	const [rosters, setRosters] = useState([]);
+
+	const getRoster = async () => {
+		const res = await axios.get('http://localhost:3000/rosters');
+		const data = res.data;
+		console.log(data);
+		setRosters(data);
+	};
+
+	const getSongs = async () => {
+		const res = await axios.get('http://localhost:3000/songs');
+		const data = res.data;
+		setSongs(data);
+	};
+
+	const getPlaylists = async () => {
+		const res = await axios.get('http://localhost:3000/playlists');
+		const data = res.data;
+		// console.log(data);
+		setPlaylists(data);
+	};
+
+	useEffect(() => {
+		getRoster();
+		getSongs();
+		getPlaylists();
+	}, []);
+
+	const songList = songs
+		? songs.map((song) => {
+				return (
+					<div>
+						<p>{song.title}</p>
+						<p>{song.artist}</p>
+					</div>
+				);
+		  })
+		: null;
+
+	const playlistList = playlists
+		? playlists.map((playlist) => {
+				return (
+					<div>
+						<p>{playlist.title}</p>
+					</div>
+				);
+		  })
+		: null;
+
+	const rosterList = rosters
+		? rosters.map((roster, index) => {
+				return (
+					<div key={index}>
+						<span>Playlist: {roster.playlist.title} --</span>
+						<span> Song: {roster.song.title}</span>
+					</div>
+				);
+		  })
+		: null;
+
+	return (
+		<div className='App'>
+			<div>
+				<b>All Songs: </b>
+				{songList}
+			</div>
+			<p>
+				<b>All Playlists: </b>
+				{playlistList}
+			</p>
+			<p>
+				<b>Playlists with songs: </b>
+				{rosterList}
+			</p>
+		</div>
+	);
 }
 
 export default App;
